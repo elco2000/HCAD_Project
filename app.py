0516a7c128dc6449c1b6665cee7eddb249dc53a7
Elco Mussert <elcomussert@gmail.com>
2022-11-02 21:05:59 +0100

    Added a form placeholder. And makes a working test for the algoritme.

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import joblib
+import numpy as np
+import sklearn
 
 app = Flask(__name__)
 
@@ -10,7 +13,14 @@ def index():  # put application's code here
 
 @app.route('/form')
 def form():
-    return render_template('form.html')
+    try:
+        model = joblib.load("Notebooks/Completed_model.pkl")
+        test_pred = np.array([2, 5, 6, 0, 1, 0, 1, 0, 1]).reshape(1, -1)
+        predicted_burnoutrate = model.predict(test_pred)
+        print(predicted_burnoutrate)
+        return render_template('form.html', rate=float(predicted_burnoutrate))
+    except ValueError:
+        return render_template('index.html')
 
 
 @app.route('/about')
